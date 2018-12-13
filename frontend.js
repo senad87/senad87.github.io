8354b1736f6d168b19f8e9c835cdaabdf5b0b0bf
Senad Mehic <senad@infaloom.com>
2018-12-13 04:03:37 +0100

    send hardcoded query

--- frontend.js
+++ frontend.js
@@ -3,15 +3,30 @@ $(function () {
   // if user is running mozilla then use it's built-in WebSocket
   window.WebSocket = window.WebSocket || window.MozWebSocket;
 
-  // var connection = new WebSocket('ws://127.0.0.1:1337');
+
   // var connection = new WebSocket('ws://localhost:5000');
+
   var connection = new WebSocket('wss://zito-alf.herokuapp.com');
 
   connection.onopen = function () {
     // connection is opened and ready to use
 
+
+
+      const query = [
+          {
+              input: ['_'],
+              output: ' '
+          },
+
+          {
+              input: ['s_a'],
+              output: 's a'
+          },
+      ];
+
     // start search
-    connection.send('upaljen');
+    connection.send(JSON.stringify(query));
 
   };
 
@@ -22,8 +37,10 @@ $(function () {
   connection.onmessage = function (message) {
     // try to decode json (I assume that each message
     // from server is json)
+    //   console.log('Od Servera', message);
     try {
-      var json = JSON.parse(message.data);
+      const json = JSON.parse(message.data);
+      console.log(json.func);
     } catch (e) {
       console.log('This doesn\'t look like a valid JSON: ',
         message.data);
