23c21a55b5aed52025457164aa0e697f30912599
Senad Mehic <senad.mehic@datumsolutions.net>
2020-01-18 01:03:37 +0100

    change port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 //setting middleware
 app.use(express.static(__dirname + '/')); //Serves resources from public folder
 
-var port = 9090;
+var port = 8080;
 app.listen(port, function() {
     console.log("started on port: ", port);
 });
